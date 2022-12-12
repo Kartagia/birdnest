6775da22f4530c7fa3a5f7b6f02ee399effe63ca
Antti Kautiainen <kautsu@elisanet.fi>
2022-12-12 15:21:04 +0200

    Building basic java servlet.
    Building basic java servlet and directory structure for the project.

---
+++ src/main/java/BirdnestServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The servlet performing the generation of the requests.
+ */
+public class BirdnestServlet extends HttpServlet {
+    
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BirdnestServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at:").append(request.getContextPath());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
