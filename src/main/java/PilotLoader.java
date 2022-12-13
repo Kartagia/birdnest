bca4010f8c051508798362f71ee11fefc663f9f8
Antti Kautiainen <kautsu@elisanet.fi>
2022-12-13 15:39:55 +0200

    Implementing Pilot, preparing DataSource for REST
    	modified:   src/main/java/PilotLoader.java
        * Restructuruing the pilot loader to suit generalization of the
        REST client with future superclass.
        * The loading of the Json data is handled with bean methods of
        the pilot reporting the invalid or read only keys of the pilot.
    
    	modified:   src/main/java/DataSource.java
        DataSource was prepraed for implementation of the REST client
    
    	modified:   src/main/java/Pilot.java
        * Pilot methods for most of the properties of the pilot, and Implementing
         splitting of the name to first anme and last name.

--- src/main/java/PilotLoader.java
+++ src/main/java/PilotLoader.java
@@ -1,11 +1,22 @@
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
+import java.io.StringReader;
+import java.lang.System.Logger.Level;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.ZonedDateTime;
+import java.util.Map.Entry;
 
+import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonValue;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+
+import org.apache.commons.text.StringEscapeUtils;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.UniformInterfaceException;
@@ -18,14 +29,86 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
  */
 public class PilotLoader {
 
-    private WebResource restClientService_;
+    /**
+     * The format strign for read only porperty.
+     */
+    private static final String READ_ONLY_PROPERTY_FORMAT_STRING = "Read only property %s of %s with value %s";
 
+    /**
+     * THe format string taking property name, object name, and value as parameters.
+     */
+    private static final String UNKNOWN_PROPERTY_FORMAT_STRING = "Unknown %s property of %s with value %s";
+
+    /**
+     * THe message indicating an innocent pilot is requested.
+     */
+    private static final String INNOCENT_PILOT_MESSAGE = "The privacy protection prevents the acquisition of the pilot data";
+
+    /**
+     * The message indicating the rest client has already been initialized.
+     */
+    public static final String REST_CLIENT_ALREADY_INITIALIXED_MESSAGE = "Rest client already initialized";
+
+    /**
+     * The undefined value string.
+     */
+    public static final String UNDEFINED_VALUE_STRING = "undefined";
+
+    /**
+     * Quote a string representation, if it is present.
+     * 
+     * @param value the quoted value.
+     * @return The strign reprsentation of the given value. The value is
+     *  the string representation of the given value quoted, if the value, and
+     *  its string representation are defined. Otherwise, the string {@link #UNDEFINED_VALUE_STRING}
+     *  is returned.
+     */
+    public static String quoteIfPresent(Object value) {
+        String stringRep = value == null ? null : value.toString();
+        if (stringRep == null) {
+            return UNDEFINED_VALUE_STRING;
+        } else {
+            return String.format("\"%s\"", StringEscapeUtils.escapeJson(stringRep));
+        }
+    }
+
+    /**
+     * The rest clietn service performing the REST client request for pilot data.
+     */
+    private WebResource restClientService_ = null;
+
+    /**
+     * The expiration time of the pilot data in minutes.
+     */
     private int expirationTimeInMinutes_ = Pilot.DEFAULT_EXPIRATION_TIMEOUT;
 
-    public PilotLoader(String host, String restPath) {
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create(config);
-        restClientService_ = client.resource(UriBuilder.fromUri(host).build());
+    /**
+     * Create pilot laoder for given host and base rest path.
+     * 
+     * @param host The host of the 
+     * @param resourcePath The resource path without parameters.
+     * @throws IllegalArgumentException The given host or resource path is invalid.
+     */
+    public PilotLoader(String host, String resourcePath) throws IllegalArgumentException {
+        initRestClient(host, resourcePath);
+    }
+
+    /**
+     * Initialize rest client.
+     * 
+     * @param host The REST host.
+     * @param resourcePath The resource path to the resource.
+     * @throws IllegalArgumentException The given host or resource path was invalid.
+     * @throws IllegalStateException The rest client has pready been initialized.
+     */
+    protected void initRestClient(String host, String resourcePath) throws IllegalArgumentException, IllegalStateException {
+        if (this.restClientService_ == null) {
+            ClientConfig config = new DefaultClientConfig();
+            Client client = Client.create(config);
+            restClientService_ = client.resource(UriBuilder.fromUri(host).build()).path(resourcePath);    
+        } else {
+            throw new IllegalStateException(REST_CLIENT_ALREADY_INITIALIXED_MESSAGE);
+        }
     }
 
     /**
@@ -39,23 +122,38 @@ public class PilotLoader {
     public Pilot getPilot(String droneSerial, ZonedDateTime violationTime, double violationDistance)
             throws IllegalArgumentException, IOException {
         if (!validViolationTime(violationTime)) {
-            throw new IllegalArgumentException("The privacy protection prevents the acquisition of the pilot data");
+            throw new IllegalArgumentException(INNOCENT_PILOT_MESSAGE);
         }
 
         try {
-            Pilot pilot = restClientService_.path(droneSerial).accept(MediaType.APPLICATION_JSON).get(Pilot.class);
-            pilot.setDistance(violationDistance);
+            String pilotString = restClientService_.path(droneSerial).accept(MediaType.APPLICATION_JSON).get(String.class);
+            JsonObject json = Json.createReader(new StringReader(pilotString)).readObject();
+            Pilot pilot = new Pilot();
+            for (Entry<String, JsonValue> entry: json.entrySet()) {
+                try {
+                    Method method = new PropertyDescriptor(entry.getKey(), Pilot.class).getWriteMethod();
+                    if (method == null) {
+                        System.getLogger(Pilot.class.getName()).log(Level.INFO, READ_ONLY_PROPERTY_FORMAT_STRING, 
+                        quoteIfPresent(entry.getKey()), "Pilot", quoteIfPresent(entry.getValue()));    
+                    } else {
+                        method.invoke(pilot, entry.getValue());
+                    }
+                } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
+                    // The property is not supported by the bean.
+                    System.getLogger(Pilot.class.getName()).log(Level.INFO, UNKNOWN_PROPERTY_FORMAT_STRING, 
+                    quoteIfPresent(entry.getKey()), "Pilot", quoteIfPresent(entry.getValue()));
+                }
+                
+            }
+            pilot.setClosestDistanceToNest(violationDistance);
             pilot.setDroneSerial(droneSerial);
             pilot.setViolationTime(violationTime);
-            if (pilot.isValid()) {
-                return pilot;
-            } else {
-                throw new IllegalArgumentException("Invalid pilot");
-            }
+            pilot.build();
+            return pilot;
         } catch (UniformInterfaceException httpError) {
-            throw new java.io.EOFException("Http error" + httpError.getMessage());
+            throw new java.io.EOFException("Http error: " + quoteIfPresent(httpError.getMessage()));
         } catch (ClientHandlerException streamCorrupted) {
-            throw new java.io.StreamCorruptedException("Invalid response content " + streamCorrupted.getMessage());
+            throw new java.io.StreamCorruptedException("Invalid response content " + quoteIfPresent(streamCorrupted.getMessage()));
         }
     }
 
